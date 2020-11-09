bf423c0aee68c763d9d8c650643e422d946c62b0
KyllianGamer <kylleke@telenet.be>
2020-11-09 16:47:17 +0100

    Discord Bot

--- index.js
+++ index.js
@@ -1,11 +1,24 @@
 const express = require('express');
+const config = require('./config.json');
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
 
 var app = express();
 app.use(express.json());
 
 app.post('/', function(req, res) {
     console.log(req.body);
+    data = req.body;
+    if (data.type==="export") {
+      let channel = client.channels.cache.get(config.exports);
+      channel.send(data.actionPlayer + ' exported!')
+    }
 });
 
 console.log('Listening on 8888');
 app.listen(process.env.PORT || 8888);
+client.login(config.TOKEN);
